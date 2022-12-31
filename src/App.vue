6d801ea62144a794b3ff8be8202124ac309036f1
Q-T5 <bikathimartin@outlook.com>
2022-12-31 22:23:10 +0300

    Add components to viewport

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,43 @@
   <div class="bg-red-500 w-full h-screen">
     <h1 class="text-3xl">App Says: {{ hi }}</h1>
   </div>
-  <div class="bg-blue-500 w-full h-screen">
-    <h1 class="text-3xl">Hello World B</h1>
+  <div class="w-full h-screen flex flex-col">
+    <div class="w-full h-[7%] shadow-md flex justify-between px-4">
+      <button 
+        v-for="(button, index) in buttons" :key="index"
+        @click="component = button.component">
+        {{ button.text }}
+      </button>
+    </div>
+    <div class="w-full h-[93%]">
+      <component :is="component" class="w-full h-full" />
+    </div>
   </div>
 </template>
 
 <script lang>
-import { ref } from 'vue';
+import { ref } from 'vue'
+import WordComp from './components/WordComp.vue'
+import SentenceComp from './components/SentenceComp.vue'
+import ParagraphComp from './components/ParagraphComp.vue'
 
 export default {
   name: "App",
+  components: {
+    WordComp, SentenceComp, ParagraphComp
+  },
   setup: function() {
     // reactive data
     const hi = ref("Hello World");
+    const component = ref("WordComp");
+    const buttons = ref([
+      { text: "Words", component: "WordComp" },
+      { text: "Sentences", component: "SentenceComp" },
+      { text: "Paragraphs", component: "ParagraphComp" },
+    ]);
 
     return {
-      hi
+      hi, component, buttons
     }
   }
 }
