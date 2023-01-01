01b3693c908d7c93624bfe9cc80a7bed3fc4fb1f
Q-T5 <bikathimartin@outlook.com>
2023-01-01 21:37:26 +0300

    Add basic component logic

--- src/components/ParagraphComp.vue
+++ src/components/ParagraphComp.vue
@@ -17,20 +17,53 @@
 <template>
     <div class="flex">
         <div class="flex flex-col w-1/3 justify-center px-2 space-y-2">
-            <div class="controls-div">
-                <h1>Controls</h1>
+            <div class="controls-div flex flex-col space-y-1">
+                <h1 class="text-center text-xl">Options</h1>
+                <div class="flex flex-col">
+                    <div class="flex flex-col items-start justify-start space-x-2">
+                        <label>Number of Paragraphs: {{ numberOfParagraphs }}</label>
+                        <input type="range" min="1" max="5" v-model="numberOfParagraphs" class="daisyui-range daisyui-range-sm" />
+                    </div>
+                    <div class="flex items-center space-x-2">
+                        <label>Disable Backspace/ Delete: </label>
+                        <input type="checkbox" class="daisyui-checkbox border-gray-500 daisyui-checkbox-sm"
+                        v-model="disableErasure" />
+                    </div>
+                </div>
+                <div class="w-full flex justify-center">
+                    <div class="daisyui-tooltip daisyui-tooltip-right" data-tip="Generate New Words">
+                        <button class="daisyui-btn rounded-full daisyui-btn-sm w-fit" @click="generate">Generate</button>
+                    </div>
+                </div>
             </div>
             <timer-comp />
         </div>
-        <div class="working-div">
-            <h1>some heading</h1>
+        <div class="working-div flex space-y-1">
+            <div class="h-3/4 w-full max-w-full flex flex-wrap max-h-[75%] overflow-y-scroll">
+                <span v-for="(word, index) in words" :key="index" :class="word.class">
+                    {{ word.word }} &nbsp;
+                </span>
+            </div>
+            <div class="h-1/4 flex justify-center">
+                <textarea 
+                    type="text" 
+                    class="user-input" 
+                    placeholder="Start Typing"
+                    rows="6"
+                    v-model="userInput"
+                    @keydown.delete="preventErase"
+                    @keydown.space="validateProgress"
+                    :disabled="baseArticle === '' "
+                    :maxlength="baseArticle.length"></textarea>
+            </div>
         </div>
     </div>
 </template>
 
 <script lang="js">
 import TimerComp from './TimerComp.vue'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import { paragraph, article } from 'txtgen'
 
 export default {
     name: "ParagraphComp",
@@ -40,6 +73,11 @@ export default {
     setup: function() {
         // reactive data
         const gameState = ref('pause');
+        const numberOfParagraphs = ref(1);
+        const baseArticle = ref("");
+        const userInput = ref("");
+        const wordsEntered = ref(0);
+        const disableErasure = ref(false);
 
         // functions
         function toogleGameState() {
@@ -47,9 +85,53 @@ export default {
                 gameState.value = "play" : gameState.value = "pause" 
         }
 
-        return {
-            gameState, toogleGameState
+        function generate() {
+            wordsEntered.value = 0;
+            if(numberOfParagraphs.value === 1) {
+                baseArticle.value = paragraph();
+            } else if(numberOfParagraphs.value > 1) {
+                baseArticle.value = article([numberOfParagraphs.value]);
+            }
         }
+
+        function preventErase(event) {
+            if(disableErasure.value === true) {
+                return event.preventDefault();
+            }
+        }
+
+        function validateProgress() {
+            wordsEntered.value++;
+            const lastWord = userInput.value.split(" ").pop();
+            if(lastWord === words[wordsEntered.value - 1].word) {
+                words[wordsEntered.value - 1].class = "text-green-500";
+            } else {
+                words[wordsEntered.value - 1].class = "text-red-500";
+            }
+        }
+
+        // computed
+        const words = computed(() => {
+            const wordsArray = baseArticle.value.split(" ");
+            const modifiedWordsArray = [];
+            
+            wordsArray.forEach((word) => {
+                modifiedWordsArray.push({
+                    word: word,
+                    class: ""
+                });
+            });
+            
+            return modifiedWordsArray;
+        });
+
+        return {
+            gameState, toogleGameState, numberOfParagraphs, generate, baseArticle, userInput,
+            words, preventErase, validateProgress, userEnteredWords, disableErasure
+        }
+    },
+    mounted: function() {
+        // console.log(article([2]));
     }
 }
 </script>
